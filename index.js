6ac564b884b8579032e4e89af76fbbe7b3b76d0b
Anh Nguyen <112703772+anhnt184@users.noreply.github.com>
2023-06-11 09:27:53 +0300

    Added Exercises 3.13.-3.14

--- index.js
+++ index.js
@@ -1,27 +1,161 @@
-const express = require('express')
-const app = express()
-
+require('dotenv').config();
+const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Person = require('./models/person');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'));
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+app.use(requestLogger);
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
+});
+
+app.get('/info', (req, res) => {
+  Person.countDocuments({}, (err, count) => {
+    if (err) {
+      res.status(500).send({ error: 'Internal server error' });
+    } else {
+      const currentTime = new Date();
+      const htmlContent = `
+        <div>
+          <p>Phone book has info for ${count} people</p>
+          <p>${currentTime}</p>
+        </div>
+      `;
+      res.send(htmlContent);
+    }
+  });
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log('Error fetching person from database:', error.message);
+      res.status(500).send({ error: 'Internal server error' });
+    });
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      if (result) {
+        res.status(204).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log('Error deleting person from database:', error.message);
+      res.status(500).send({ error: 'Internal server error' });
+    });
+});
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body;
+  if (!name || !number) {
+    return res.status(400).json({
+      error: 'Name or number missing',
+    });
+  }
+// Create new person
+const newPerson = new Person({
+  name: name,
+  number: number,
+});
+
+newPerson.save()
+  .then((savedPerson) => {
+    console.log(`Added ${savedPerson.name} number ${savedPerson.number} to phonebook`);
+    res.json(savedPerson);
+  })
+  .catch((error) => {
+    console.log('Error saving person to database:', error.message);
+    res.status(500).send({ error: 'Internal server error' });
+  });
+});
+
+app.put('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const { name, number } = req.body;
+  Person.findByIdAndUpdate(id, { name, number }, { new: true })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        console.log(`Updated ${updatedPerson.name} number ${updatedPerson.number} in phonebook`);
+        res.json(updatedPerson);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log('Error updating person in database:', error.message);
+      res.status(500).send({ error: 'Internal server error' });
+    });
+});
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// 404 Unknown Endpoint
+app.use((req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' });
+});
 
-app.use(cors())
+// Connect to MongoDB and start the server
+const url = process.env.MONGODB_URI;
+console.log('connecting to', url);
 
-app.use(morgan('tiny'));
+// Connect to Database
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
+// Disconnect from Database
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB connection closed');
+    process.exit(0);
+  });
+});
 // Token for logging person data
 morgan.token('person', (req) => {
   if (req.method === 'POST') {
@@ -29,113 +163,10 @@ morgan.token('person', (req) => {
   }
   return '';
 });
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'));
-app.use(express.json());
-app.use(requestLogger)
 
-let persons = [
-        {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-        },
-        {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-        },
-        {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-        },
-        {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-        }
-]
+app.use(unknownEndpoint);
 
-// app.use(express.static('build'))
-app.use(express.static(path.join(__dirname, 'build')));
-
-// app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
-app.get('/info', (req, res) => {
-    const entryCount = persons.length
-    const currentTime = new Date()
-    const htmlContent = `
-    <div>
-      <p>Phone book has info for ${entryCount} people</p>
-      <p>${currentTime}</p>
-    </div>
-    `
-    res.send(htmlContent)
-  })
-
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-  
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  
-  })
-
-  app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-  
-    res.status(204).end()
-  })
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/persons', (req, res) => {
-  const body = req.body
-  if (!body.name || !body.number) {
-    return res.status(400).json({ 
-      error: 'name or number missing' 
-    })
-  }
-
-  const existingPerson = persons.find(p => p.name === body.name)
-
-  if (existingPerson) {
-    return res.status(400).json({
-        error: { error: 'Name must be unique' }
-    })
-  }
-
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  }
-
-  persons = persons.concat(person)
-  res.json(person)
-})
-
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
